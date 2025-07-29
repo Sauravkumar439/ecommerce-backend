b0c99eb0a212df240aa8031fa63c2525607e906a
Saurav Kumar <sauravkumar439@gmail.com>
2025-07-29 15:24:57 +0530

    Fix: Correct User model path case in adminMiddleware

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const adminMiddleware = async (req, res, next) => {
   try {
